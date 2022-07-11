3e3f447894bfe128ce8eb003554375991aaf9bed
siddhantadhav <isiddhantadhav@gmail.com>
2022-07-11 09:54:58 +0530

    data cannot be inserted in database

--- src/app.js
+++ src/app.js
@@ -1,25 +1,48 @@
-const express = require("express");
-const app = express();
-const path = require("path")
-require("./db/conn");
-const Register = require("./models/submit")
-const port = process.env.PORT || 3000;
+var express=require("express");
+var bodyParser=require("body-parser");
 
-const static_path = path.join(__dirname,"../public");
-// console.log(static_path);
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bombay_realty');
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+	console.log("connection succeeded");
+})
+
+var app=express()
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.post('/submit', function(req,res){
+	var name = req.body.name;
+	var email =req.body.email;
+	var contact =req.body.contact;
+
+	var data = {
+		"name": name,
+		"email":email,
+		"contact":contact
+	}
+db.collection('details').insertOne(data,function(err, collection){
+		if (err) throw err;
+		console.log("Record inserted Successfully");
+			
+	});
+		
+	return res.redirect('Successful');
+})
 
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-
-app.use(express.static(static_path));
-
-app.get("/", (req, res) => {
-    res.send("index")
-});
+app.get('/',function(req,res){
+res.set({
+	'Access-control-Allow-Origin': '*'
+	});
+return res.redirect('index.html');
+}).listen(3000)
 
 
-
-app.listen(port, () => {
-    console.log(`server running`);
-});
+console.log("server listening at port 3000");
