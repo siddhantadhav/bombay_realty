35cc7392b90c8ee9978ec3c6943c00ae69e502fc
siddhantadhav <isiddhantadhav@gmail.com>
2022-07-12 14:08:41 +0530

    working

--- src/app.js
+++ src/app.js
@@ -1,48 +1,35 @@
-var express=require("express");
-var bodyParser=require("body-parser");
+const express = require('express')
+const path = require('path');
+require('./db/conn');
+const Register = require('./models/registers')
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bombay_realty');
-var db=mongoose.connection;
-db.on('error', console.log.bind(console, "connection error"));
-db.once('open', function(callback){
-	console.log("connection succeeded");
+const app = express();
+const port = process.env.PORT || 3000;
+
+var file = path.join(__dirname, '../public');
+
+app.use(express.static(file))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// // console.log(file)
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(file));
+});
+
+app.post('/', async(req, res) => {
+  try {
+    
+
+  } catch (error) {
+    res.status(400),send(error);
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-
-var app=express()
-
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-app.post('/submit', function(req,res){
-	var name = req.body.name;
-	var email =req.body.email;
-	var contact =req.body.contact;
-
-	var data = {
-		"name": name,
-		"email":email,
-		"contact":contact
-	}
-db.collection('details').insertOne(data,function(err, collection){
-		if (err) throw err;
-		console.log("Record inserted Successfully");
-			
-	});
-		
-	return res.redirect('Successful');
-})
-
-
-app.get('/',function(req,res){
-res.set({
-	'Access-control-Allow-Origin': '*'
-	});
-return res.redirect('index.html');
-}).listen(3000)
-
-
-console.log("server listening at port 3000");
---
+++ src/models/registers.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true
+
+    },
+    email: {
+        type: String,
+        required:true
+
+    },
+    contact: {
+        type: Number,
+        required:true
+
+    }
+})
+
+const Register =  new mongoose.model("Register", userSchema);
+
+module.exports = Register;
